af606582e149bef0e9869ab935f12ff39059e35f
lilpolymath <ayofavour7@gmail.com>
2020-03-04 12:40:02 +0100

    osumi

--- src/index.js
+++ src/index.js
@@ -37,6 +37,6 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen({ port: process.env.PORT || 4040 }, () => {
+app.listen({ port: 8000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 });
